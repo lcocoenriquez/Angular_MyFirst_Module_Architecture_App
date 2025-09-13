daf7ee9954d761e8bd3551dcb87b6caff261de5d
lcocoenriquez <lcocoenriquez@gmail.com>
2025-09-13 16:28:33 +0000

    my_first_commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,53 +1,45 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  # We use the unstable channel to get the latest nodejs
+  channel = "unstable";
+
+  # Add required packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_22
+    pkgs.nodePackages."@angular/cli"
   ];
-  # Sets environment variables in the workspace
-  env = {};
+
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    # Recommended extensions for Angular development
     extensions = [
-      # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
+      "angular.ng-template"
     ];
-    # Enable previews
+    workspace = {
+      # Run this command when the workspace is created
+      onCreate = {
+        install-deps = "npm install --prefix my-angular-app";
+      };
+      # onStart is not needed as the preview starts the server
+      onStart = {};
+    };
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        web = {
+          # The command to start the angular development server
+          command = [
+            "npm"
+            "run"
+            "start"
+            "--"
+            "--port"
+            "$PORT"
+            "--host"
+            "0.0.0.0"
+            "--disable-host-check"
+          ];
+          manager = "web";
+          cwd = "my-angular-app";
+        };
       };
     };
   };
